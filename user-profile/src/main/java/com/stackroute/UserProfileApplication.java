dcfd6fd5c782e3b1062a768d4ce55fe07f1d7e0a
uday n <udayn612@gmail.com>
2019-01-28 15:35:03 +0530

    modified dependencies

--- user-profile/src/main/java/com/stackroute/UserProfileApplication.java
+++ user-profile/src/main/java/com/stackroute/UserProfileApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class UserProfileApplication<EnableEurekaClient> {
+public class UserProfileApplication {
 
 	public static void main(String[] args) {
 
